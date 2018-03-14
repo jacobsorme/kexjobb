aea3f094bd300a8ec6beb9d231bdd96c3bc903fe
sorme <sorme@kth.se>
2018-03-14 14:39:36 +0100

    Spiral & random, vinkelräknare funkar

--- cleaner.js
+++ cleaner.js
@@ -12,6 +12,7 @@ class Cleaner {
     this.ang=ang;
     this.speed = speed;
     this.trace=[[x,y]];
+    this.totalAng = 0;
   }
   render(){
     ctx.globalAlpha=0.1;
@@ -19,36 +20,36 @@ class Cleaner {
     //ctx.lineCap="round";
     ctx.strokeStyle="#000";
     ctx.lineWidth=2*this.radius;
-    for(let i = 1; i <this.trace.length;i++){
+    let i =this.trace.length-1;
       ctx.beginPath();
       ctx.moveTo(this.trace[i-1][0],this.trace[i-1][1]);
       ctx.lineTo(this.trace[i][0],this.trace[i][1]);
       ctx.stroke();
-    }
-    ctx.globalAlpha=1;
-    ctx.lineWidth=1;
-    ctx.beginPath();
-    ctx.translate(this.x,this.y);
-    ctx.rotate(this.ang);
-    ctx.arc(0,0,this.radius,0,2*Math.PI);
-    //ctx.fill();
-    ctx.stroke();
-    ctx.rotate(-this.ang);
-    ctx.translate(-this.x,-this.y);
 
 
-    ctx.beginPath();
-    ctx.strokeStyle="#999";
-    ctx.translate(this.startx,this.starty);
-    ctx.rotate(this.ang);
-    ctx.arc(0,0,this.radius,0,2*Math.PI);
-    //ctx.fill();
-    ctx.stroke();
-    ctx.rotate(-this.ang);
-    ctx.translate(-this.startx,-this.starty);
   }
   rotate(ang){
+    this.totalAng += Math.abs(this.ang - ang);
     this.ang = (ang) % (2*Math.PI);
+  }
+  bounceRight(){
+    if(this.ang<Math.PI/2) this.rotate(Math.PI/2 + Math.random()*Math.PI/2);
+    else this.rotate(Math.PI + Math.random()*Math.PI/2);
+  }
+  bounceLeft(){
+    if(this.ang>Math.PI/2&&this.ang<Math.PI) this.rotate(Math.PI/2 - Math.random()*Math.PI/2);
+    else this.rotate(3*2*Math.PI/4 + Math.random()*Math.PI/2);
+  }
+  bounceTop(){
+    console.log("BOUNCE: "+this.ang);
+
+    if(this.ang<2*Math.PI&&this.ang>3*2*Math.PI/4) this.rotate(Math.random()*Math.PI/2);
+    else this.rotate(Math.PI/2+Math.random()*Math.PI/2);
+    console.log(" --> "+this.ang);
+  }
+  bounceBottom(){
+    if(this.ang<Math.PI/2) this.rotate(3*2*Math.PI/4+Math.random()*Math.PI/2);
+    else this.rotate(Math.PI+Math.random()*Math.PI/2)
   }
   move(){
     //this.trace.pop();
@@ -56,63 +57,25 @@ class Cleaner {
     this.x += (this.speed*Math.cos(this.ang));
     this.y += (this.speed*Math.sin(this.ang));
     if(this.x+this.radius>canvas.width) {
-      // q1 & q4
-      if(this.ang<Math.PI/2) this.rotate(Math.PI/2 + Math.random()*Math.PI/2);
-      else this.rotate(Math.PI + Math.random()*Math.PI/2);
-      //this.ang = Math.PI/2 + Math.random()*Math.PI;
+      this.bounceRight();
       this.x=canvas.width-this.radius;
       this.trace.push([this.x,this.y]);
     }
     else if(this.x-this.radius<0) {
-      // q3 & q2
-      if(this.ang>Math.PI/2&&this.ang<Math.PI) this.rotate(Math.PI/2 - Math.random()*Math.PI/2);
-      else this.rotate(3*2*Math.PI/4 + Math.random()*Math.PI/2)
-      //this.ang = -Math.PI/2 + Math.random()*Math.PI;
+      this.bounceLeft();
       this.x=this.radius+0;
       this.trace.push([this.x,this.y]);
     }
     else if(this.y-this.radius<0) {
-      // q1 & q2
-      if(this.ang<2*Math.PI&&this.ang>3*2*Math.PI/4) this.rotate(Math.random()*Math.PI/2);
-      else this.rotate(Math.PI/2+Math.random()*Math.PI/2);
-      //this.ang = Math.random()*Math.PI;
+      this.bounceTop();
       this.y=this.radius;
       this.trace.push([this.x,this.y]);
     }
     else if(this.y+this.radius>canvas.height) {
-      // q4 & q3
-      if(this.ang<Math.PI/2) this.rotate(3*2*Math.PI/4+Math.random()*Math.PI/2);
-      else this.rotate(Math.PI+Math.random()*Math.PI/2)
-      //this.ang = Math.PI+Math.random()*Math.PI;
+      this.bounceBottom();
       this.y=canvas.height-this.radius;
       this.trace.push([this.x,this.y]);
     }
     this.trace.push([this.x,this.y]);
   }
-}
-
-
-var c = new Cleaner(canvas.width*Math.random(),canvas.height*Math.random(),3,"#5f1",Math.PI*2*Math.random(), 1);
-
-setInterval(run,30);
-
-function run(){
-  ctx.globalAlpha=1;
-  ctx.strokeStyle="#000";
-  ctx.lineWidth=2;
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.strokeRect(0,0,canvas.width,canvas.height);
-  c.move();
-  c.render();
-
-  if(Math.random()<0.5) console.log(pixels(ctx));
-
-
-}
-
-function pixels(ctx){
-  count=0;
-  var imgData=ctx.getImageData(0,0,canvas.width,canvas.height);
-  for (var i=0;i<imgData.data.length;i+=4) if(imgData.data[i+3]>0)count++;
-  return count/imgData.data.length*4;
 }
---
+++ spiral.js
@@ -0,0 +1,47 @@
+class Spiral extends Cleaner {
+  constructor(x,y,radius,color,ang, speed){
+    super(x,y,radius,color,ang,speed);
+    this.inSpiral = false;
+    this.turn = 0.6;
+    this.margin = 40;
+  }
+  resetSpiralParams(){
+    this.turn = 0.6;
+    this.inSpiral = false;
+  }
+  move(){
+    this.x += (this.speed*Math.cos(this.ang));
+    this.y += (this.speed*Math.sin(this.ang));
+    if( this.inSpiral || (this.x>this.margin && this.y>this.margin && this.x<canvas.width-this.margin && this.y<canvas.height-this.margin)){
+      this.rotate(this.ang+=this.turn);
+      this.turn=this.turn/(1.01-(0.01-Math.abs(this.turn)/20));
+      this.inSpiral=true;
+    }
+    if(this.x+this.radius>canvas.width) {
+      this.bounceRight();
+      this.x=canvas.width-this.radius;
+      this.trace.push([this.x,this.y]);
+      this.resetSpiralParams();
+    }
+    else if(this.x-this.radius<0) {
+      this.bounceLeft();
+      this.x=this.radius+0;
+      this.trace.push([this.x,this.y]);
+      this.resetSpiralParams();
+    }
+    else if(this.y-this.radius<0) {
+      this.bounceTop();
+      this.y=this.radius;
+      this.trace.push([this.x,this.y]);
+      this.resetSpiralParams();
+    }
+    else if(this.y+this.radius>canvas.height) {
+      this.bounceBottom();
+      this.y=canvas.height-this.radius;
+      this.trace.push([this.x,this.y]);
+      this.resetSpiralParams();
+    }
+    this.trace.push([this.x,this.y]);
+  }
+
+}

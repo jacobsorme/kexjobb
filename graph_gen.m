89208cdc1037eb9544aa6d3c00039682df9e5204
sorme <sorme@kth.se>
2018-05-24 11:05:19 +0200

    Add pictures for presentation

--- graph_gen.m
+++ graph_gen.m
@@ -41,35 +41,35 @@ x = (1:95)';
 hold on; 
 %%% UPDATES 
 % plot(x,random_updates_e,'b--','DisplayName','Random Elongated Room')
-%plot(x,random_updates_s,'b','DisplayName','Random Square Room')
-%fillPlot(random_updates_s,random_upd_s_sd,x,'b'); 
+plot(x,random_updates_s,'b','DisplayName','Random Square Room')
+fillPlot(random_updates_s,random_upd_s_sd,x,'b'); 
 % 
 %plot(x,snaking_updates_e,'r--','DisplayName','Snaking Elongated Room')
-%plot(x,snaking_updates_s,'r','DisplayName','Snaking Square Room');
-%fillPlot(snaking_updates_s,snaking_upd_s_sd,x,'r'); 
+plot(x,snaking_updates_s,'r','DisplayName','Snaking Square Room');
+fillPlot(snaking_updates_s,snaking_upd_s_sd,x,'r'); 
 
 % plot(x,spiral_updates_e,'g--','DisplayName','Spiral Elongated Room')
-%plot(x,spiral_updates_s,'g','DisplayName','Spiral Square Room')
-%fillPlot(spiral_updates_s,spiral_upd_s_sd,x,'g');
+plot(x,spiral_updates_s,'g','DisplayName','Spiral Square Room')
+fillPlot(spiral_updates_s,spiral_upd_s_sd,x,'g');
 
 %%% TURNS 
 %plot(x,random_turns_e,'b--','DisplayName','Random Elongated Room')
 %fillPlot(random_turns_e,random_trn_e_sd,x,'b'); 
 
-plot(x,random_turns_s,'b','DisplayName','Random Square Room')
-fillPlot(random_turns_s,random_trn_s_sd,x,'b'); 
+%plot(x,random_turns_s,'b','DisplayName','Random Square Room')
+%fillPlot(random_turns_s,random_trn_s_sd,x,'b'); 
 
 
 %plot(x,snaking_turns_e,'r--','DisplayName','Snaking Elongated Room')
 %fillPlot(snaking_turns_e,snaking_trn_e_sd,x,'r');
-plot(x,snaking_turns_s,'r','DisplayName','Snaking Square Room'); 
-fillPlot(snaking_turns_s,snaking_trn_s_sd,x,'r'); 
+%plot(x,snaking_turns_s,'r','DisplayName','Snaking Square Room'); 
+%fillPlot(snaking_turns_s,snaking_trn_s_sd,x,'r'); 
 
 %plot(x,spiral_turns_e,'g--','DisplayName','Spiral Elongated Room')
 %fillPlot(spiral_turns_e,spiral_trn_e_sd,x,'g'); 
 
-plot(x,spiral_turns_s,'g','DisplayName','Spiral Square Room')
-fillPlot(spiral_turns_s,spiral_trn_s_sd,x,'g'); 
+%plot(x,spiral_turns_s,'g','DisplayName','Spiral Square Room')
+%fillPlot(spiral_turns_s,spiral_trn_s_sd,x,'g'); 
 
 %hold off
 
@@ -77,12 +77,12 @@ legend(gca,'show','location','northwest')
 
 %legend('DisplayName','Snaking Square Room')
 legend('boxoff')
-title('Snaking, Spiral & Random - Turns 2000 runs average')
-ylabel('Turns')
+title('Snaking, Spiral & Random - Updates 2000 runs average')
+ylabel('Updates')
 xlabel('% of area coverage')
 hold off;
 
-axis([0 100 0 250])
+%axis([0 100 0 250])
 
 % x = 1 : 20;
 % list = [1,2,3,4,3,2,4,5,6,7,6,5,4,3,2,4,5,6,7,8];
